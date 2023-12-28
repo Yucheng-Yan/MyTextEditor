8c2c8c1d7e7177c4f56e6271305b0e6bfed751b5
yucheng <imyanyucheng@gmail.com>
2023-12-28 11:29:46 +1100

    press q to quit

--- kilo.c
+++ kilo.c
@@ -2,6 +2,9 @@
 
 int main() {
   char c;
+  /* Because the return value of read is how many bytes did it read, and "1" after "&c" specifies that the read() function only reads one byte once a time. Hence, this read() function will always return 1*/
+
+  /* The second parameter of read() is a pointer pointing to an address that will store the content read from STDIN_FILENO*/
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
